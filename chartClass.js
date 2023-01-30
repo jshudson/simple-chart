24934baa0aa235ad718078198246f6a7462bb7fd
Jeremy Hudson <Jeremy.Hudson@Milliken.com>
2023-01-30 17:14:09 -0500

    zoomable

--- chartClass.js
+++ chartClass.js
@@ -1,3 +1,4 @@
+const SVGNS = 'http://www.w3.org/2000/svg'
 class Chart {
     constructor(div) {
 
@@ -5,10 +6,11 @@ class Chart {
 
         this.plotRange = {
             x1: 0,
-            x2: 100,
-            y1: 0,
-            y2: 100
+            x2: 6,
+            y1: -3,
+            y2: 20
         }
+
         this.plotScreenDimensions = {
             pad: 10,
             width: this.element.offsetWidth,
@@ -16,41 +18,73 @@ class Chart {
         }
 
         this.data = [];
+        this.strokeWidth = 3;
 
         this.initializeResizeObserver();
         this.resizeObserver.observe(this.element);
 
-        const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-        svg.setAttribute('width', this.plotScreenDimensions.width)
-        svg.setAttribute('height', this.plotScreenDimensions.height)
+        this.chart = this.element.appendChild(document.createElementNS(SVGNS, 'svg'));
+        this.chart.setAttribute('width', this.plotScreenDimensions.width)
+        this.chart.setAttribute('height', this.plotScreenDimensions.height)
 
-        const plotClip = this.appendNewElement(svg, 'clipPath', {
-            id: 'plot-clip'
-        })
+        this.plotClip = this.appendNewElement(this.chart, 'clipPath', { id: 'plot-clip' }, SVGNS)
 
-        const clipRect = plotClip.appendChild(this.createRectangle(0, this.plotScreenDimensions.pad, '200vh', '200vw', 'clip-rect'))
-        console.log(clipRect);
-        svg.innerHTML += `
-        <g id="plot"></g>
-        <g id="rect"></g>
-        `
-        this.chart = this.element.appendChild(svg);
-        this.plot = document.getElementById("plot");
+        this.clipRect = this.plotClip.appendChild(
+            this.createRectangle(
+                this.plotScreenCoordinates.x1 - this.strokeWidth,
+                this.plotScreenCoordinates.y1 - this.strokeWidth,
+                this.plotScreenCoordinates.x2 - this.plotScreenDimensions.pad + this.strokeWidth,
+                this.plotScreenCoordinates.y2 - this.plotScreenDimensions.pad + this.strokeWidth,
+                'clip-rect'))
 
+        this.plot = this.appendNewElement(this.chart, 'g', { id: 'plot' }, SVGNS)
+
+        this.zoomRect = {
+            group: this.appendNewElement(this.chart, 'g', { id: 'zoom-rect' }, SVGNS),
+            rectangle: undefined,
+            active: false,
+            x1: 0,
+            x2: 0,
+            y1: 0,
+            y2: 0
+        }
+
+        this.appendNewElement(this.zoomRect.group, 'g', { id: 'moose' }, SVGNS)
+
+        //event handlers
+        this.chart.onmousedown = this.handleClick.bind(this);
+        this.chart.onmouseup = this.handleMouseUp.bind(this);
+        this.chart.onmousemove = this.handleMouseMove.bind(this);
+        this.chart.onmouseleave = this.handleMouseLeave.bind(this);
+        this.chart.ondblclick = this.handleDoubleClick.bind(this);
     }
 
-    // #region Chart Functions
+    // #region Chart Update Functions
 
     resize() {
+        this.updatePlotScreenDimensions();
+        this.resizeChart();
+        this.resizeClip();
+        if (this.hasData) this.updatePlot();
+        return true
+    }
+
+    updatePlotScreenDimensions() {
         this.plotScreenDimensions = {
             ...this.plotScreenDimensions,
             width: this.element.offsetWidth,
             height: this.element.offsetHeight,
         }
-        this.chart.style.width = this.plotScreenDimensions.width
-        this.chart.style.height = this.plotScreenDimensions.height
-        if (this.hasData) this.updatePlot();
-        return true
+    }
+
+    resizeChart() {
+        this.chart.setAttribute('width', this.plotScreenDimensions.width)
+        this.chart.setAttribute('height', this.plotScreenDimensions.height)
+    }
+
+    resizeClip() {
+        this.clipRect.setAttribute('width', Math.max(0, this.plotScreenCoordinates.x2 - this.plotScreenDimensions.pad + this.strokeWidth))
+        this.clipRect.setAttribute('height', Math.max(0, this.plotScreenCoordinates.y2 - this.plotScreenDimensions.pad + this.strokeWidth))
     }
 
     /**
@@ -58,26 +92,27 @@ class Chart {
      */
     updatePlot() {
         if (!this.hasData) return;
-        this.plotRange = this.getDataRange(this.data[0]);
+        // this.plotRange = this.getDataRange(this.data[0]);
         this.plot.innerHTML = this.getPathString(this.data[0]);
     }
     //#endregion
 
     // #region DOM utilities
-    appendNewElement(parent, tagName, attributes) {
-        const child = document.createElement(tagName)
+    appendNewElement(parent, tagName, attributes, NS) {
+        const child = NS ? document.createElementNS(NS, tagName) : document.createElement(tagName)
+        console.log(child);
         Object.keys(attributes).forEach(key => {
             child.setAttribute(key, attributes[key])
         })
         parent.appendChild(child);
         return child;
     }
-    createRectangle(x, y, height, width, id) {
-        const rect = document.createElement('rect');
+    createRectangle(x, y, width, height, id) {
+        const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
         rect.setAttribute('x', x);
         rect.setAttribute('y', y);
-        rect.setAttribute('height', height);
         rect.setAttribute('width', width);
+        rect.setAttribute('height', height);
         if (id) rect.setAttribute('id', id)
         return rect;
     }
@@ -95,6 +130,7 @@ class Chart {
         this.hasData = true;
         this.data = [...this.data, points];
         console.log(this.data);
+        console.log(this.getDataRange(this.data[0]));
     }
 
     /**
@@ -176,10 +212,20 @@ class Chart {
      * @param {number} scaledMax - Maximum value of the target scale
      * @returns {Array} Array values scaled to the provided coordinate system
      */
-    swapScale(array, originalMin, originalMax, scaledMin, scaledMax) {
-        return array.map(e => ((e - originalMin) * (scaledMax - scaledMin) / (originalMax - originalMin) + scaledMin))
+    scaleArray(array, originalMin, originalMax, scaledMin, scaledMax) {
+        return array.map(e => this.scalePoint(e, originalMin, originalMax, scaledMin, scaledMax))
     }
-
+    scalePoint(point, originalMin, originalMax, scaledMin, scaledMax) {
+        return (point - originalMin) * (scaledMax - scaledMin) / (originalMax - originalMin) + scaledMin
+    }
+    scaleRectange(rectangle, originalCoords, scaledCoords) {
+        return {
+            x1: this.scalePoint(rectangle.x1, originalCoords.x1, originalCoords.x2, scaledCoords.x1, scaledCoords.x2),
+            x2: this.scalePoint(rectangle.x2, originalCoords.x1, originalCoords.x2, scaledCoords.x1, scaledCoords.x2),
+            y1: this.scalePoint(rectangle.y2, originalCoords.y2, originalCoords.y1, scaledCoords.y1, scaledCoords.y2),
+            y2: this.scalePoint(rectangle.y1, originalCoords.y2, originalCoords.y1, scaledCoords.y1, scaledCoords.y2),
+        }
+    }
     /**
      * Scale points object to SVG screen coordinates
      * @param {Object} points - collection of xy data
@@ -190,10 +236,18 @@ class Chart {
     scalePointsToScreen(points) {
         const screenCoords = this.plotScreenCoordinates
 
-        const scaleX = this.swapScale(points.x, this.plotRange.x1, this.plotRange.x2, screenCoords.x1, screenCoords.x2);
-        const scaleY = this.swapScale(points.y, this.plotRange.y1, this.plotRange.y2, screenCoords.y2, screenCoords.y1);
+        const scaleX = this.scaleArray(points.x, this.plotRange.x1, this.plotRange.x2, screenCoords.x1, screenCoords.x2);
+        const scaleY = this.scaleArray(points.y, this.plotRange.y1, this.plotRange.y2, screenCoords.y2, screenCoords.y1);
 
         return { x: scaleX, y: scaleY }
+    }
+    sortCoords({ x1, x2, y1, y2 }) {
+        return {
+            x1: Math.min(x1, x2),
+            x2: Math.max(x1, x2),
+            y1: Math.min(y1, y2),
+            y2: Math.max(y1, y2)
+        }
     }
     //#endregion
 
@@ -241,6 +295,19 @@ class Chart {
     }
     //#endregion
 
+    // #region Zoom Rectangle Handling
+    updateZoomRectangle() {
+        console.log(this.zoomRect.rectangle);
+        const width = Math.abs(this.zoomRect.x2 - this.zoomRect.x1)
+        const height = Math.abs(this.zoomRect.y2 - this.zoomRect.y1)
+        const x = Math.min(this.zoomRect.x1, this.zoomRect.x2);
+        const y = Math.min(this.zoomRect.y1, this.zoomRect.y2);
+        this.zoomRect.rectangle.setAttribute('width', width)
+        this.zoomRect.rectangle.setAttribute('height', height)
+        this.zoomRect.rectangle.setAttribute('x', x)
+        this.zoomRect.rectangle.setAttribute('y', y)
+    }
+    // #endregion
 
     // #region Events
     initializeResizeObserver() {
@@ -250,6 +317,64 @@ class Chart {
                 this.resize();
             };
         })
+    }
+
+    handleClick(e) {
+        const loc = { x: e.offsetX, y: e.offsetY }
+
+        this.zoomRect.active = true;
+        this.zoomRect.x1 = this.zoomRect.x2 = loc.x;
+        this.zoomRect.y1 = this.zoomRect.y2 = loc.y;
+        this.zoomRect.rectangle = this.zoomRect.group.appendChild(this.createRectangle(loc.x, loc.y, 0, 0))
+        this.zoomRect.rectangle.setAttribute('id', 'rect')
+    }
+
+    handleMouseMove(e) {
+        e.preventDefault()
+        if (this.zoomRect.active) {
+            this.zoomRect.x2 = e.offsetX
+            this.zoomRect.y2 = e.offsetY
+            this.updateZoomRectangle();
+        }
+    }
+
+    handleMouseLeave(e) {
+        e.preventDefault()
+        if (this.zoomRect.active) {
+            this.zoomRect.rectangle.remove();
+            this.zoomRect.active = false;
+        }
+    }
+
+    handleMouseUp(e) {
+        e.preventDefault()
+
+        const width = Number(this.zoomRect.rectangle.getAttribute('width'))
+        const height = Number(this.zoomRect.rectangle.getAttribute('height'))
+        this.zoomRect.rectangle.remove()
+
+        if (width === 0 || height === 0) {
+            this.zoomRect.active = false;
+            return;
+        }
+
+
+        if (this.zoomRect.active) {
+            this.zoomRect.active = false;
+            const screenCoords = this.plotScreenCoordinates
+            const sortedCoords = this.sortCoords(this.zoomRect)
+
+            const scaledZoom = this.scaleRectange(sortedCoords, screenCoords, this.plotRange)
+
+            this.plotRange = { ...scaledZoom }
+            this.updatePlot();
+        }
+    }
+
+    handleDoubleClick(e) {
+        e.preventDefault();
+        this.plotRange = this.getDataRange(this.data[0]);
+        this.updatePlot();
     }
     // #endregion
 }
